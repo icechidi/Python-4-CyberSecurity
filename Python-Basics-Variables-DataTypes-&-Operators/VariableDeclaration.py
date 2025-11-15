bbbf2df5396ccbbb50030c609bbf35ac88fbd871
Ice Chidi <icechidi3@gmail.com>
2025-11-15 23:46:39 +0200

    Variable initialization

---
+++ Python-Basics-Variables-DataTypes-&-Operators/VariableDeclaration.py
@@ -0,0 +1,5 @@
+#1. Write a Python program to declare variables of different data types 
+# (int, float, string, Boolean, and complex), assign values to them, and display their types using the type()function.
+
+intVar = 10
+floatVar = 10.5
