87a9321fe9963f3af7e36a344ebe2e777f633ccc
Ice Chidi <icechidi3@gmail.com>
2025-11-16 23:08:42 +0200

    Positive, Negative and zero values

---
+++ Python-Basics-Variables-DataTypes-&-Operators/positiveNegZero.py
@@ -0,0 +1,12 @@
+import re
+
+num = int(input("Enter any number to check: "))
+
+if num >=1 :
+    print("You have entered a positive number")
+
+elif num < 0:
+    print("You have entered a negative number")
+    
+else:
+    print("The given number is Zero")
